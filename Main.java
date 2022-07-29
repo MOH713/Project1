0acec17a18c86ee11f8622e878a97900c1cd4c15
MOHIT KUMAR <69642298+MOH713@users.noreply.github.com>
2022-07-29 15:28:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package practice_session;
+
+public class Main {	
+		/*Enter your desired Directory path */
+		public static final String path = "C:\\Users\\shubh\\Downloads\\Main"; 
+		
+		public static void main(String[] args) {
+			Menus menu = new Menus();
+			menu.introScreen();
+			menu.mainMenu();
+		}
+}
